33470eae371285ee6ae9a28092459ce150779cdb
holequoctin <holequocti@gmail.com>
2023-03-16 13:19:44 +0700

    predict age

--- src/App.js
+++ src/App.js
@@ -4,22 +4,25 @@ import Axios from "axios"
 function App() {
   
   //thằng dependencies không phải là một giá trị mà nó là một cái biến, useEffect sẽ được gọi lại khi các biến dependencies thay đổi
-  const [catFact,setCatFact] = useState("")
-  const [generate,setGenerate] = useState(true)
-  useEffect(()=>{
-    Axios.get("https://catfact.ninja/fact").then((res) => {
-    setCatFact(res.data.fact)
-  })
-  },[generate])
+  const [name,setName] = useState("")
+  const [age,setAge] = useState(0)
+  const fetchData = () => {
+    Axios.get(`https://api.agify.io/?name=${name}`).then((res) => {
+      setAge(res.data.age)
+    })
+  }
   return <div className = "App">
     <div>
-      <button onClick={() => {
-        setGenerate(!generate)
-      }}>
-        Generate Cat Fact
+      <input type="text" placeholder='Enter name' onChange={(event) => {
+          setName(event.target.value)
+      }}></input>
+    </div>
+    <div>
+      <button onClick={fetchData}>
+        Predict Age
       </button>
     </div>
-    <h1>{catFact}</h1>
+    <h1>{age? age:"No data"}</h1>
   </div>
 }
 export default App;
